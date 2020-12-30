362d12e2987e9e4cbf7565aa7d15ad15d5ec2f31
Rishabh Kunwar <rishabh.kunwar04@gmail.com>
2020-12-30 20:53:52 +0530

    ..
    Signed-off-by: Rishabh Kunwar <rishabh.kunwar04@gmail.com>

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express=require('express')
 const app=express()
 const path=require('path')
 
+const SERVER_PORT=process.env.PORT || 3333
+
 app.use('/public',express.static(path.join(__dirname,'public')))
 
 app.get('/',(req,res)=>{
   res.send("backend hosting")
 })
 
-app.listen(3333,()=>{
-  console.log('http://localhost:3333')
+app.listen(SERVER_PORT,()=>{
+  console.log('server started')
 })
